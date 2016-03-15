0b09008bac3ee61d275fd41488d0c1e5753639ba
Eli <55drk1@gmail.com>
2016-03-15 21:00:32 +0100

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+/*
+* Dependencias
+*/
+var gulp = require('gulp'),
+
+gulp.task('build-js', ['clean'], function() {  
+    var b = browserify({
+        entries: './js/app.js',
+        debug: true,
+        paths: ['./js/controllers', './js/services', './js/directives'],
+        transform: [ngAnnotate]
+    });
+ 
+    return b.bundle()
+        .pipe(source('bundle.js'))
+        .pipe(buffer())
+        .pipe(cachebust.resources())
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(uglify())
+        .on('error', gutil.log)
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./dist/js/'));
+});
+
+/*
+*Default task
+*/
+gulp.task('default', ['watch','browser-sync']);
+
+
+
+
+
