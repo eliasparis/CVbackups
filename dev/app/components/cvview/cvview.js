86fe5087f0def124a32f3fe47195720927a9da75
Eli <55drk1@gmail.com>
2016-03-25 20:59:13 +0100

    Working on cvview

--- dev/app/components/cvview/cvview.js
+++ dev/app/components/cvview/cvview.js
@@ -1,7 +1,7 @@
 angular
 	.module('app.cvview',['ui.router'])
     .controller('CvviewCtrl', ['$stateParams', '$scope', function($stateParams, $scope) {
+   		
    		$scope.cvtitle = $stateParams.cvtitle;
-   		console.log($stateParams.cvtitle);
-
+    
     }]);
