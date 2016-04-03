aadf65d744cafec163fb5e0b3cd43f5fe02154e8
Eli <55drk1@gmail.com>
2016-04-03 20:54:48 +0200

    CV form object

--- dev/app/components/cvadd/cvadd.js
+++ dev/app/components/cvadd/cvadd.js
@@ -1,5 +1,9 @@
 angular
-	.module('app.cvadd',['ui.router'])
-	.controller('CvaddCtrl',['$scope', function($scope) {
+	.module('app.cvadd',['ui.router', 'ngMessages'])
+	.controller('CvaddCtrl',['$scope', function($scope, $event) {
 		$scope.hello = 'here we will add a cv';
+		
+		this.onSubmit = function(){
+			
+		};
 	}]);
